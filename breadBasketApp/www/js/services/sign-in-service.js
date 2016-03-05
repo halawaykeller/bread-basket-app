684b5f5e812d4ea2f81a8567291b7fe26c1526e6
Bradley Brown <tall.white.ninja@gmail.com>
2016-03-05 13:10:12 -0700

    log-in half-way works

--- breadBasketApp/www/js/services/sign-in-service.js
+++ breadBasketApp/www/js/services/sign-in-service.js
@@ -7,13 +7,21 @@
 
 //"signinService refers to what's in the signin-controller. 
 app.service("SigninService", function($http){
-	this.SIGNIN_ENDPOINT = "https://breadbasketapp.com/php/controllers/sign-in-controller.php";
+	//this.SIGNIN_ENDPOINT = "/php/controllers/sign-in-controller.php";
+	this.GET_ENDPOINT = "/php/api/organization/";
 
 	this.signin = function(signinData) { //signinData from the signin-controller and signin-modal
-		return($http.post(this.SIGNIN_ENDPOINT, signinData)
-				.then(function(reply) {
-					return(reply.data);
-				}));
+		//get request for the sole purpose of obtaining a cookie
+		//this is TOTALLY a hack, and it's terrible and wrong and we shouldn't do it
+		return($http.get(this.GET_ENDPOINT))
+				.then(function (){
+					console.log("got something");
+					return($http.post("/php/controllers/sign-in-controller.php", signinData)
+							.then(function(reply) {
+								return(reply.data);
+							}));
+
+				});
 	};
 });
 
