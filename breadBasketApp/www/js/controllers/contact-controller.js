cc68b484db4ea0056eb3308f7635066fb332417e
Tamra Fenstermaker <fenstermaker505@gmail.com>
2016-03-12 11:53:13 -0700

    Removed $uibModal

--- breadBasketApp/www/js/controllers/contact-controller.js
+++ breadBasketApp/www/js/controllers/contact-controller.js
@@ -1,21 +1,21 @@
-app.controller("ContactController", ["$scope", "$uibModal", "ContactService", function($scope, $uibModal, ContactService) {
+app.controller("ContactController", ["$scope", "ContactService", function($scope, ContactService) {
 	$scope.contactData = {};
 
-	$scope.openContactModal = function() {
-		var ContactModalInstance = $uibModal.open({
-			templateUrl: "../../js/views/contact-modal.php",
-			controller: "ContactModal",
-			resolve: {
-				contactData: function() {
-					return ($scope.contactData);
-				}
-			}
-		});
-		ContactModalInstance.result.then(function(contactData) {
-			$scope.contactData = contactData;
-			ContactService.contact(contactData);
-
-
-		});
-	};
+	//$scope.openContactModal = function() {
+	//	var ContactModalInstance = $uibModal.open({
+	//		templateUrl: "../../js/views/contact-modal.php",
+	//		controller: "ContactModal",
+	//		resolve: {
+	//			contactData: function() {
+	//				return ($scope.contactData);
+	//			}
+	//		}
+	//	});
+	//	ContactModalInstance.result.then(function(contactData) {
+	//		$scope.contactData = contactData;
+	//		ContactService.contact(contactData);
+    //
+    //
+	//	});
+	//};
 }]);
