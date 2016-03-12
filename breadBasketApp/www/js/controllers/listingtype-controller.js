bca0d76b5344cbb0edf8a3058cb517628c64eab5
Tamra Fenstermaker <fenstermaker505@gmail.com>
2016-03-12 11:42:30 -0700

    Removed $uibModal

--- breadBasketApp/www/js/controllers/listingtype-controller.js
+++ breadBasketApp/www/js/controllers/listingtype-controller.js
@@ -5,7 +5,7 @@
  * contributing code from Misquote @author Dylan McDonald
  */
 
-app.controller("ListingTypeController", ["$scope", "$uibModal", "ListingTypeService", "AlertService", function($scope, ListingTypeService, AlertService) {
+app.controller("ListingTypeController", ["$scope", "ListingTypeService", "AlertService", function($scope, ListingTypeService, AlertService) {
 	//add as needed
 	$scope.listingTypes = [];
 	$scope.alerts = [];
@@ -16,17 +16,17 @@ app.controller("ListingTypeController", ["$scope", "$uibModal", "ListingTypeServ
 	 * opens new listingType modal and adds sends listingType to the listingType API
 	 */
 
-	$scope.openListingModal = function() {
-		var ListingModalInstance = $uibModal.open({
-			templateUrl: "../../js/views/newlisting-modal.php",
-			controller: "ListingModal",
-			resolve: {
-				listingType: function() {
-					return ($scope.listingType);
-				}
-			}
-		});
-	}
+	//$scope.openListingModal = function() {
+	//	var ListingModalInstance = $uibModal.open({
+	//		templateUrl: "../../js/views/newlisting-modal.php",
+	//		controller: "ListingModal",
+	//		resolve: {
+	//			listingType: function() {
+	//				return ($scope.listingType);
+	//			}
+	//		}
+	//	});
+	//}
 
 	//get listingType from API REST Endpoint
 	//come back for the other gets
@@ -95,41 +95,41 @@ app.controller("ListingTypeController", ["$scope", "$uibModal", "ListingTypeServ
 		}
 	};
 
-	// delete a listing type (DELETE)
-	$scope.deleteListingType = function(listingTypeId) {
-		// create a modal to ask for confirmation. should be wired to listing modal
-		var message = "Do you really want to delete this Listing Type?";
-		var modalHtml = '<div class="modal-body">' + message + '</div><div class="modal-footer"><button class="btn btn-primary" ng-click="yes()">Yes</button><button class="btn btn-warning" ng-click="no()">No</button></div>';
-		var modalInstance = $uibModal.open({
-			template: modalHtml,
-			controller:ModalInstanceCtrl
-		});
-
-		//if yes is selected, delete the listingType
-		modalInstance.result.then(function() {
-			ListingTypeService.destroy(listingTypeId)
-					.then(function(result) {
-						if(result.data.status === 200) {
-					$scope.alerts[0] = {type: "success", msg: result.data.message};
-				} else {
-				$scope.alerts[0] = {type: "danger", msg: result.data.message};
-				}
-			})
-		});
-	};
+	//// delete a listing type (DELETE)
+	//$scope.deleteListingType = function(listingTypeId) {
+	//	// create a modal to ask for confirmation. should be wired to listing modal
+	//	var message = "Do you really want to delete this Listing Type?";
+	//	var modalHtml = '<div class="modal-body">' + message + '</div><div class="modal-footer"><button class="btn btn-primary" ng-click="yes()">Yes</button><button class="btn btn-warning" ng-click="no()">No</button></div>';
+	//	var modalInstance = $uibModal.open({
+	//		template: modalHtml,
+	//		controller:ModalInstanceCtrl
+	//	});
+    //
+	//	//if yes is selected, delete the listingType
+	//	modalInstance.result.then(function() {
+	//		ListingTypeService.destroy(listingTypeId)
+	//				.then(function(result) {
+	//					if(result.data.status === 200) {
+	//				$scope.alerts[0] = {type: "success", msg: result.data.message};
+	//			} else {
+	//			$scope.alerts[0] = {type: "danger", msg: result.data.message};
+	//			}
+	//		})
+	//	});
+	//};
 }]);
 
 
 
-//modal instance controller for deletion prompt
-var ModalInstanceCtrl = function($scope, $uibModalInstance) {
-	$scope.yes = function() {
-		$uibModalInstance.close();
-	};
-	$scope.no = function() {
-		$uibModalInstance.dismiss('cancel');
-	};
-};
+////modal instance controller for deletion prompt
+//var ModalInstanceCtrl = function($scope, $uibModalInstance) {
+//	$scope.yes = function() {
+//		$uibModalInstance.close();
+//	};
+//	$scope.no = function() {
+//		$uibModalInstance.dismiss('cancel');
+//	};
+//};
 
 
 
