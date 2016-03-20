5a8157292cf4faf5058e0759668ac39f111309e0
Tamra Fenstermaker <fenstermaker505@gmail.com>
2016-03-20 12:12:50 -0600

    Removed modal and made signup similar to sign in. Still have the Cannot GET/sign-up.html error

--- breadBasketApp/www/js/controllers/signup-controller.js
+++ breadBasketApp/www/js/controllers/signup-controller.js
@@ -1,26 +1,29 @@
-app.controller("SignupController", ["$scope", "$uibModal", "$window", "AlertService", "SignupService", function($scope, $uibModal, $window, AlertService, SignupService) {
-	$scope.signupData = {};
+app.controller("SignupController", ["$scope", "ionicLoading", "$uibModal", "$window", "$state", "AlertService", "SignupService", function($scope, $ionicLoading, $uibModal, $window, $state, AlertService, SignupService) {
+	//$scope.signupData = {};
+	$scope.alerts = [];
 
-	$scope.openSignupModal = function () {
-		var signupModalInstance = $uibModal.open({
-			templateUrl: "../../js/views/signup-modal.php",
-			controller: "SignupModal",
-			resolve: {
-				signupData: function () {
-					return($scope.signupData);
-				}
-			}
-		});
-		signupModalInstance.result.then(function (signupData) {
-			$scope.signupData = signupData;
-			SignupService.signup(signupData)
+	//$scope.openSignupModal = function () {
+	//	var signupModalInstance = $uibModal.open({
+	//		templateUrl: "../../js/views/signup-modal.php",
+	//		controller: "SignupModal",
+	//		resolve: {
+	//			signupData: function () {
+	//				return($scope.signupData);
+	//			}
+	//		}
+	//	});
+	//	signupModalInstance.result.then(function (signupData) {
+		$scope.openSignupModal = function(data) {
+			$ionicLoading.sho();
+			$scope.signupData = data;
+			SignupService.signup(data)
 					.then(function(reply) {
 						if(reply.status === 200) {
 							AlertService.addAlert({type: "success", msg: reply.message});
 							if(signupData.orgType === "G") {
-								$window.location.assign("../../php/template/login-landing-giver.php");
+								$state.go("tab.dashboard");
 							} else if(signupData.orgType === "R") {
-								$window.location.assign("../../php/template/login-landing-page.php");
+								$state.go("tab.dashboard");
 							}
 						} else {
 							AlertService.addAlert({type: "danger", msg: reply.message});
@@ -28,6 +31,5 @@ app.controller("SignupController", ["$scope", "$uibModal", "$window", "AlertServ
 					});
 		}, function() {
 			$scope.signupData = {};
-		});
 	};
 }]);
